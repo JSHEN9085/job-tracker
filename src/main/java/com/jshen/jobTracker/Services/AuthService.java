1fba9182008a555caf484bd2f0c570169fefd8b6
JSHEN9085 <jshen9085@gmail.com>
2021-09-27 22:46:15 -0400

    pw encry

--- src/main/java/com/jshen/jobTracker/Services/AuthService.java
+++ src/main/java/com/jshen/jobTracker/Services/AuthService.java
@@ -4,6 +4,7 @@ import com.jshen.jobTracker.dto.RegisterRequest;
 import com.jshen.jobTracker.models.User;
 import com.jshen.jobTracker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +13,17 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public void signup(RegisterRequest registerRequest){
         User user = new User();
         user.setUsername(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword( registerRequest.getPassword()) );
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password){
+        return passwordEncoder.encode(password);
     }
 }
