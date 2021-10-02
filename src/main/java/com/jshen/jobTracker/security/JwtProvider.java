8afef2b650f0776a43bad337000fe9bd39e275c2
JSHEN9085 <jshen9085@gmail.com>
2021-10-02 15:18:18 -0400

    receive token from request

--- src/main/java/com/jshen/jobTracker/security/JwtProvider.java
+++ src/main/java/com/jshen/jobTracker/security/JwtProvider.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Claims;
 
 import javax.annotation.PostConstruct;
 import java.security.Key;
@@ -26,5 +27,19 @@ public class JwtProvider {
                 .setSubject(principal.getUsername())
                 .signWith(Keys.secretKeyFor(SignatureAlgorithm.HS512))
                 .compact();
+    }
+
+    public boolean validateToken(String jwt){
+        Jwts.parser().setSigningKey(key).parseClaimsJws(jwt);
+        return true;
+    }
+
+    public String getUsernameFromJwt(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
     }
 }
